ee498b8fa52e7efe190543205e8bfbe8c68d38cc
VladcatUA <vladislav2002vladislav2002@gmail.com>
2023-07-06 00:00:04 +0300

    add-some-elements-final

--- src/js/modal-menu.js
+++ src/js/modal-menu.js
@@ -2,6 +2,9 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeMenuBtn1 = document.querySelector('.js-close-menu-1');
+  const closeMenuBtn2 = document.querySelector('.js-close-menu-2');
+  const closeMenuBtn3 = document.querySelector('.js-close-menu-3');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -17,6 +20,9 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn1.addEventListener('click', toggleMenu);
+  closeMenuBtn2.addEventListener('click', toggleMenu);
+  closeMenuBtn3.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
